659d552570f99cf912cd1e641b2276ad52a28ea5
Bryan Ho <hob@wycliffeassociates.org>
2019-08-14 19:30:30 -0400

    Run Parallel Linting

--- USFMToolsSharp.Linter/USFMLinter.cs
+++ USFMToolsSharp.Linter/USFMLinter.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 using USFMToolsSharp.Linter.LinterModules;
 using USFMToolsSharp.Linter.Models;
 using USFMToolsSharp.Models.Markers;
@@ -16,14 +17,29 @@ namespace USFMToolsSharp.Linter
             new UnpairedEndMarkers(),
             new MissingTableRows()
         };
-        public List<LinterResult> Lint(USFMDocument input)
+        public async Task<List<LinterResult>> Lint(USFMDocument input)
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            List<LinterResult> output = new List<LinterResult>();
+            output = await PerformLintParallelAsync(input);
+            watch.Stop();
+            var ellapsedMs = watch.ElapsedMilliseconds;
+            Console.WriteLine(ellapsedMs);
+            return output;
+        }
+        private async Task<List<LinterResult>> PerformLintParallelAsync(USFMDocument root)
         {
             List<LinterResult> output = new List<LinterResult>();
-            foreach(var linter in linters)
+            List<Task<List<LinterResult>>> tasks = new List<Task<List<LinterResult>>>();
+            foreach (var linter in linters)
             {
-                output.AddRange(linter.Lint(input));
+                tasks.Add(Task.Run(() => linter.Lint(root)));
             }
-
+            var results = await Task.WhenAll(tasks);
+            foreach (var item in results)
+            {
+                output.AddRange(item);
+            }
             return output;
         }
     }
