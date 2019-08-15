ad72083c7b45ac089d4274cc6b29045f213e55a8
Bryan Ho <hob@wycliffeassociates.org>
2019-08-15 11:07:31 -0400

    Base structure for metadata

--- USFMToolsSharp.Linter/Models/Versification.cs
+++ USFMToolsSharp.Linter/Models/Versification.cs
@@ -4,7 +4,20 @@ using System.Text;
 
 namespace USFMToolsSharp.Linter.Models
 {
-    class Class1
+    class Versisification
     {
+        public class Chapter
+        {
+            public int ChapterNum { get; set; }
+            public int VerseTotal { get; set; }
+
+        }
+        public class Book
+        {
+            public List<Chapter> Chapters { get; set; }
+            public string BookID { get; set; }
+            public string BookName { get; set; }
+            public int TotalChapters { get; set; }
+        }
     }
 }
