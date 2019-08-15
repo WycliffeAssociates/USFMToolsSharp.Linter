919490b2ae4648a1f0aee9a2a242dc41bc944361
Bryan Ho <hob@wycliffeassociates.org>
2019-08-15 09:11:12 -0400

    Add Checks for missing properties

--- USFMToolsSharp.Linter/USFMLinter.cs
+++ USFMToolsSharp.Linter/USFMLinter.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using USFMToolsSharp.Linter.LinterModules;
 using USFMToolsSharp.Linter.Models;
+using USFMToolsSharp.LinterModules;
 using USFMToolsSharp.Models.Markers;
 
 namespace USFMToolsSharp.Linter
@@ -14,7 +15,44 @@ namespace USFMToolsSharp.Linter
             new VerseMarkerValidation(),
             new MissingEndMarkers(),
             new UnpairedEndMarkers(),
-            new MissingTableRows()
+            new MissingTableRows(),
+            // Check null/empty marker properties
+            new MissingCAMarkerProps(),
+            new MissingCLMarkerProps(),
+            new MissingCMarkerProps(),
+            new MissingCPMarkerProps(),
+            new MissingFKMarkerProps(),
+            new MissingFMarkerProps(),
+            new MissingFRMarkerProps(),
+            new MissingFVMarkerProps(),
+            new MissingHMarkerProps(),
+            new MissingIDEMarkerProps(),
+            new MissingIDMarkerProps(),
+            new MissingIMTMarkerProps(),
+            new MissingIOTMarkerProps(),
+            new MissingISMarkerProps(),
+            new MissingMRMarkerProps(),
+            new MissingMSMarkerProps(),
+            new MissingMTMarkerProps(),
+            new MissingQACMarkerProps(),
+            new MissingQAMarkerProps(),
+            new MissingREMMarkerProps(),
+            new MissingSMarkerProps(),
+            new MissingSPMarkerProps(),
+            new MissingSTSMarkerProps(),
+            new MissingTOC1MarkerProps(),
+            new MissingTOC2MarkerProps(),
+            new MissingTOC3MarkerProps(),
+            new MissingTOCA1MarkerProps(),
+            new MissingTOCA2MarkerProps(),
+            new MissingTOCA3MarkerProps(),
+            new MissingUSFMMarkerProps(),
+            new MissingVAMarkerProps(),
+            new MissingVPMarkerProps(),
+            new MissingWMarkerProps(),
+            new MissingXMarkerProps(),
+            new MissingXOMarkerProps(),
+
         };
         public List<LinterResult> Lint(USFMDocument input)
         {
