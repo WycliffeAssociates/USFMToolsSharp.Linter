38bce9127190d5fc27104ae0f3529dec21212b3a
Bryan Ho <hob@wycliffeassociates.org>
2019-08-14 20:05:37 -0400

    Support checks for unordered verses

--- USFMToolsSharp.Linter/LinterModules/UnorderedVerses.cs
+++ USFMToolsSharp.Linter/LinterModules/UnorderedVerses.cs
@@ -11,22 +11,59 @@ namespace USFMToolsSharp.Linter.LinterModules
     {
         public List<LinterResult> Lint(USFMDocument input)
         {
-            List<int> chapterIndecies = new List<int>();
             List<LinterResult> results = new List<LinterResult>();
 
             foreach (Marker marker in input.Contents)
             {
                 if (marker is CMarker)
                 {
-                    results.AddRange(ValidateVerseOrder(marker));
+                    results.AddRange(ValidateVerseOrder((CMarker)marker));
                 }
             }
             return results;
         }
-        public List<LinterResult> ValidateVerseOrder(Marker input)
+        public List<LinterResult> ValidateVerseOrder(CMarker input)
         {
+            List<int> verseNumbers = new List<int> { 0 };
             List<LinterResult> results = new List<LinterResult>();
+            foreach(VMarker verse in input.GetChildMarkers<VMarker>())
+            {
+                if (string.IsNullOrEmpty(verse.StartingVerse.ToString()))
+                {
+                    if (verseNumbers.Count > 0)
+                    {
+                        if (verse.StartingVerse != verseNumbers[verseNumbers.Count - 1] + 1)
+                        {
+                            results.Add(new LinterResult
+                            {
+                                Position = verse.Position,
+                                Level = LinterLevel.Warning,
+                                Message = $"Chapter {input.Number}: Verse {verse.StartingVerse} is not in the correct order."
+                            });
+                        }
+                    }
+                    verseNumbers.Add(verse.EndingVerse);
+
+                }
+                else
+                {
+                    if (verseNumbers.Count > 0)
+                    {
+                        if (Int32.Parse(verse.VerseNumber) != verseNumbers[verseNumbers.Count - 1] + 1)
+                        {
+                            results.Add(new LinterResult
+                            {
+                                Position = verse.Position,
+                                Level = LinterLevel.Warning,
+                                Message = $"Chapter {input.Number}: Verse {verse.VerseNumber} is not in the correct order."
+                            });
+                        }
+                    }
+                    verseNumbers.Add(Int32.Parse(verse.VerseNumber));
+                }
+            }
             return results;
         }
+
     }
 }
