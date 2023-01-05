6090bde913e669d892fecdad5ba0c71fda1cdcfa
Craig Oliver <craig.robert.oliver@gmail.com>
2023-01-05 16:31:36 -0500

    Multiple unknown markers

--- USFMToolsSharp.Linter.Test/USFMLinterTest.cs
+++ USFMToolsSharp.Linter.Test/USFMLinterTest.cs
@@ -28,10 +28,31 @@ namespace USFMToolsSharp.Linter.Test
             USFMDocument doc = parser.ParseFromString("Invalid marker: \\cheese");
             USFMLinter linter = new USFMLinter();
             List<LinterResult> results = linter.Lint(doc);
+
             Assert.AreEqual(1, results.Count);
+
             LinterResult warning = results[0];
             Assert.AreEqual("The marker cheese is unknown", warning.Message);
             Assert.AreEqual(16, warning.Position);
+        }
+
+        [TestMethod]
+        public void TestMultipleUnknownMarkers()
+        {
+            USFMParser parser = new();
+            USFMDocument doc = parser.ParseFromString("Invalid marker 1: \\cheese Invalid marker 2: \\apple");
+            USFMLinter linter = new USFMLinter();
+            List<LinterResult> results = linter.Lint(doc);
+
+            Assert.AreEqual(2, results.Count);
+
+            LinterResult warning0 = results[0];
+            Assert.AreEqual("The marker cheese is unknown", warning0.Message);
+            Assert.AreEqual(18, warning0.Position);
+
+            LinterResult warning1 = results[1];
+            Assert.AreEqual("The marker apple is unknown", warning1.Message);
+            Assert.AreEqual(44, warning1.Position);
         }
 
 
