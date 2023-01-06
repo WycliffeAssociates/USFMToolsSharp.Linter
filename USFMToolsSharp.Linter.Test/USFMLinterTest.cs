e233472f26c66c5aa6424af0d610e18eeeb4467e
Craig Oliver <craig.robert.oliver@gmail.com>
2023-01-06 13:26:20 -0500

    Merged tests back together, removed test unable to be run

--- USFMToolsSharp.Linter.Test/USFMLinterTest.cs
+++ USFMToolsSharp.Linter.Test/USFMLinterTest.cs
@@ -110,21 +110,6 @@ namespace USFMToolsSharp.Linter.Test
         }
 
         [TestMethod]
-        public void TestInvalidDoubleVerseRange()
-        {
-            USFMParser parser = new();
-            USFMDocument doc = parser.ParseFromString("spacing before \\v 1-2-3");
-            USFMLinter linter = new USFMLinter();
-            List<LinterResult> results = linter.Lint(doc);
-
-            Assert.AreEqual(1, results.Count);
-
-            LinterResult warning = results[0];
-            Assert.AreEqual("Verse number is invalid", warning.Message);
-            Assert.AreEqual(15, warning.Position);
-        }
-
-        [TestMethod]
         public void TestReverseOrderInvalidVerseRange()
         {
             USFMParser parser = new();
@@ -188,7 +173,7 @@ namespace USFMToolsSharp.Linter.Test
         public void TestMultipleInvalidVerseMarkers()
         {
             USFMParser parser = new();
-            USFMDocument doc = parser.ParseFromString("spacing before \\v \\bd stuff \\bd* \\v 1-2-3");
+            USFMDocument doc = parser.ParseFromString("spacing before \\v \\bd stuff \\bd* \\v 01");
             USFMLinter linter = new USFMLinter();
             List<LinterResult> results = linter.Lint(doc);
 
@@ -272,5 +257,63 @@ namespace USFMToolsSharp.Linter.Test
             Assert.AreEqual("Missing opening marker for bd*", warning.Message);
             Assert.AreEqual(23, warning.Position);
         }
+
+        [TestMethod]
+        public void TestPresentEndMarker()
+        {
+            USFMParser parser = new();
+
+            foreach (string marker in pairedMarkers)
+            {
+                USFMDocument doc = parser.ParseFromString($"Text \\{marker} with end marker \\{marker}*");
+                USFMLinter linter = new();
+                List<LinterResult> results = linter.Lint(doc);
+
+                Assert.AreEqual(0, results.Count);
+            }
+
+        }
+
+        [TestMethod]
+        public void TestMissingEndMarker()
+        {
+            USFMParser parser = new();
+
+            foreach (string marker in pairedMarkers)
+            {
+                USFMDocument doc = parser.ParseFromString($"Text \\{marker} with no end marker");
+                USFMLinter linter = new();
+                List<LinterResult> results = linter.Lint(doc);
+
+                Assert.AreEqual(1, results.Count);
+
+                LinterResult warning = results[0];
+                Assert.AreEqual($"Missing closing marker for {marker}", warning.Message);
+                Assert.AreEqual(5, warning.Position);
+            }
+
+        }
+
+        [TestMethod]
+        public void TestMissingEndMarkerInsideVerse()
+        {
+            USFMParser parser = new();
+
+            foreach (string marker in pairedMarkers)
+            {
+                USFMDocument doc = parser.ParseFromString($"\\v 1 Text \\{marker} with no end marker");
+                USFMLinter linter = new();
+                List<LinterResult> results = linter.Lint(doc);
+
+                Assert.AreEqual(1, results.Count);
+
+                LinterResult warning = results[0];
+                Assert.AreEqual($"Missing closing marker for {marker}", warning.Message);
+                Assert.AreEqual(10, warning.Position);
+            }
+
+        }
+
+
     }
 }
